ceb20e5c421c672228fe6e446761b7b29fb91df2
Aleksandr <tkachenkox@yahoo.com>
2021-01-18 11:20:24 +0200

    Added postrsql

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ ARTICLES_PER_PAGE = 5
 class Config(object):
     SECRET_KEY = 'AAA'
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345@localhost/ad_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     
